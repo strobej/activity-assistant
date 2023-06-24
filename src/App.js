5a10a86e8787d5c8e2969d9932109930b2037a01
strobej <selma.trobej@outlook.com>
2023-06-24 17:49:29 +0200

    fix: added initial working API call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [currentActivity, setActivity] = useState([]);
+  const url = "http://www.boredapi.com/api/activity/";
+
+  async function getActivity() {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    setActivity(data.activity);
+  }
+
+  useEffect(() => {
+    getActivity();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h4>Testing Api calls v0.1</h4>
+      <h3>Suggested activity: {currentActivity}</h3>
     </div>
   );
 }
